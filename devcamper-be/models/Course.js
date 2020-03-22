301332638763eda11482d1a89dac388b087ee143
RylanZhou <475777146@qq.com>
2020-03-22 21:38:13 +0800

    Set calculate average cost hooks on Courses.

--- devcamper-be/models/Course.js
+++ devcamper-be/models/Course.js
@@ -39,4 +39,38 @@ const CourseSchema = new mongoose.Schema({
   }
 })
 
+// Static method to get average of course tuitions
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+  // Pipe line
+  const objArray = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' }
+      }
+    }
+  ])
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: (Math.ceil(objArray[0].averageCost) / 10) * 10
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Call getAverageCost() after adding a course
+CourseSchema.post('save', function() {
+  this.constructor.getAverageCost(this.bootcamp)
+})
+
+// Call getAverageCost() after removing a course
+CourseSchema.pre('remove', function() {
+  this.constructor.getAverageCost(this.bootcamp)
+})
+
 module.exports = mongoose.model('Course', CourseSchema)
--- devcamper-be/seeder.js
+++ devcamper-be/seeder.js
@@ -22,14 +22,15 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 )
-const courses = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
-)
+// const courses = JSON.parse(
+//   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+// )
 
 // Import data into DB
 const importData = async () => {
   try {
-    await Promise.all([Bootcamp.create(bootcamps), Course.create(courses)])
+    await Bootcamp.create(bootcamps)
+    // await Course.create(courses)
     console.log('Data imported...'.green.inverse)
     process.exit()
   } catch (error) {
@@ -40,7 +41,8 @@ const importData = async () => {
 // Delete data
 const deleteData = async () => {
   try {
-    await Promise.all([Bootcamp.deleteMany(), Course.deleteMany()])
+    await Course.deleteMany()
+    await Bootcamp.deleteMany()
     console.log('Data destroyed...'.red.inverse)
     process.exit()
   } catch (error) {
