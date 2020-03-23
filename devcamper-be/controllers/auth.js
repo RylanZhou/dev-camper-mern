40dbc34871a8deaf3799c9c3a4e4669d58b7cf90
RylanZhou <475777146@qq.com>
2020-03-23 21:25:10 +0800

    Forgot and reset password, update user info and password.

--- devcamper-be/controllers/auth.js
+++ devcamper-be/controllers/auth.js
@@ -1,4 +1,6 @@
+const crypto = require('crypto')
 const asyncHandler = require('../middleware/async')
+const sendEmail = require('../utils/sendEmail')
 const ErrorResponse = require('../utils/ErrorResponse')
 const User = require('../models/User')
 
@@ -64,6 +66,123 @@ exports.getMe = asyncHandler(async (request, response, next) => {
     success: true,
     data: user
   })
+})
+
+/**
+ * @description Forgot password
+ * @route       POST /api/vi/auth/forgot-password
+ * @access      Public
+ */
+exports.forgotPassword = asyncHandler(async (request, response, next) => {
+  const user = await User.findOne({ email: request.body.email })
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email.', 404))
+  }
+
+  // Generate reset token into the resetPasswordToken filed.
+  const resetToken = user.getResetPasswordToken()
+
+  await user.save({ validateBeforeSave: false })
+
+  // Create reset url
+  const resetUrl = `${request.protocol}://${request.get(
+    'host'
+  )}/api/v1/auth/reset-password/${resetToken}`
+
+  const message = `You are receiving this email because you (or someone) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    })
+  } catch (error) {
+    console.log(error)
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+
+    await user.save({ validateBeforeSave: false })
+
+    return next(new ErrorResponse('Email could not be sent.', 500))
+  }
+
+  response.status(200).json({
+    success: true,
+    data: 'Email Sent.'
+  })
+})
+
+/**
+ * @description Reset password
+ * @route       PUT /api/vi/auth/reset-password/:resettoken
+ * @access      Public
+ */
+exports.resetPassword = asyncHandler(async (request, response, next) => {
+  // Get hashed token
+  const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(request.params.resettoken)
+    .digest('hex')
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() }
+  })
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid Token', 400))
+  }
+
+  // Set new password
+  user.password = request.body.password
+  user.resetPasswordToken = undefined
+  user.resetPasswordExpire = undefined
+
+  await user.save()
+
+  sendTokenResponse(user, 200, response)
+})
+
+/**
+ * @description Update user details
+ * @route       PUT /api/vi/auth/update/info
+ * @access      Private
+ */
+exports.updateUserInfo = asyncHandler(async (request, response, next) => {
+  const fieldsToUpdate = {
+    name: request.body.name,
+    email: request.body.email
+  }
+  const user = await User.findByIdAndUpdate(request.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  })
+
+  response.status(200).json({
+    success: true,
+    data: user
+  })
+})
+
+/**
+ * @description Update password
+ * @route       PUT /api/vi/auth/update/password
+ * @access      Private
+ */
+exports.updatePassword = asyncHandler(async (request, response, next) => {
+  const user = await User.findById(request.user.id).select('+password')
+
+  // Check current password
+  if (!(await user.matchPassword(request.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401))
+  }
+
+  user.password = request.body.newPassword
+  await user.save()
+
+  sendTokenResponse(user, 200, response)
 })
 
 // Get token from model, create cookie and send response
--- devcamper-be/routes/auth.js
+++ devcamper-be/routes/auth.js
@@ -7,5 +7,9 @@ const router = express.Router()
 router.post('/register', authController.register)
 router.post('/login', authController.login)
 router.get('/me', protect, authController.getMe)
+router.post('/forgot-password', authController.forgotPassword)
+router.put('/reset-password/:resettoken', authController.resetPassword)
+router.put('/update/info', protect, authController.updateUserInfo)
+router.put('/update/password', protect, authController.updatePassword)
 
 module.exports = router
